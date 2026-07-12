a5f8b7e8b74569a25f674bb5b841c38233517704
TIN TIN <pepream@gmail.com>
2026-07-12 20:15:26 +0700

    Delete src directory

--- src/state.js
+++
@@ -1,5 +0,0 @@
-// In-memory per-user wizard state for the "add job" / "custom cron" flows.
-// key: telegram user id -> { step, chatId, text, ... }
-const state = new Map();
-
-module.exports = state;
